ae82ea875f3f10302d40074cced2eaef08fd83d9
laurenciopaez <laurencio314@gmail.com>
2023-07-15 17:26:00 -0300

    'Detail page: onClick '

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/cards.module.css";
+import DetailPage from "./DetailPage";
 
 const Card = ({ name, image, landingDate, platform }) => {
+  const [showDetailPage, setShownDetailPage] = useState(false);
+
+  const handleClick = () => {
+    setShownDetailPage(true);
+  };
+
+  const handleDetailPageClose = () => {
+    setShownDetailPage(false)
+  }
+
+  if (showDetailPage) {
+    return (
+      <DetailPage
+        name={name}
+        image={image}x
+        landingDate={landingDate}
+        platform={platform}
+        onClose={handleDetailPageClose}
+      />
+    );
+  }
+
   return (
     <>
-      <div className={styles.card}>
+      <div className={styles.card} onClick={handleClick}>
         <h3>{name}</h3>
         <img src={image} alt="Game" />
         <p>{landingDate}</p>
-        <p className="platform">{platform}</p>
+        <p className={styles.platform}>{platform}</p>
       </div>
     </>
   );
--- client/src/components/DetailPage.jsx
+++ client/src/components/DetailPage.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import style from '../styles/detail.module.css';
 
-function DetailPage() {
-    return (
-        <></>
-    )
+function DetailPage({ name, image, landingDate, platform, onClose }) {
+
+    const handleButtonClick = () => {
+        onClose()
+    }
+
+  return (
+    <>
+      <div className={style.detail_page}>
+        <h2 className={style.detail_page__name}>{name}</h2>
+        <div className={style.detail_page__image}>
+          <img src={image} alt="Imagen del videojuego" />
+        </div>
+        <p className={style.detail_page__landing_date}>{landingDate}</p>
+        <p className={style.detail_page__platform}>{platform}</p>
+        <button className={style.detail_page__close_button} onClick={handleButtonClick}>
+          X
+        </button>
+      </div>
+      <div className={style.background}></div>
+    </>
+  );
 }
 
 export default DetailPage;
